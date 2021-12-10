237a43dc34aa1115dcad4a72afe7281a4a2a897b
eliotheinrich <eliot.heinrich@gmail.com>
2021-12-10 12:43:48 -0500

    Added vorticity function to SquareXYModel, added plotting functions for vorticity, reorganized

--- XY/SquareXYModel.cpp
+++ XY/SquareXYModel.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <stdlib.h>
 #include <Eigen/Dense>
+#include <complex>
 #include "../XYModel.cpp"
 #include "../Utility.cpp"
 
@@ -55,6 +56,40 @@ class SquareXYModel : public XYModel {
                 }
             }
             return new_model;
+        }
+
+        inline vector<double> vorticity() {
+            float v1 = 0;
+            float v2 = 0;
+
+            vector<vector<vector<float>>> phi = vector<vector<vector<float>>>(N,
+                                                        vector<vector<float>>(N,
+                                                                vector<float>(L)));
+
+            for (int n1 = 0; n1 < N; n1++) {
+                for (int n2 = 0; n2 < N; n2++) {
+                    for (int n3 = 0; n3 < L; n3++) {
+                        phi[n1][n2][n3] = 0.;
+                        phi[n1][n2][n3] = atan2(spins[n1][n2][n3][0][1], spins[n1][n2][n3][0][0]);
+                    }
+                }
+            }
+
+            float p1; float p2; float p3; float p4;
+            float w;
+            for (int n1 = 0; n1 < N; n1++) {
+                for (int n2 = 0; n2 < N; n2++) {
+                    for (int n3 = 0; n3 < L; n3++) {
+                        p1 = phi[n1][n2][n3]; p2 = phi[(n1+1)%N][n2][n3];
+                        p3 = phi[(n1+1)%N][(n2+1)%N][n3]; p4 = phi[n1][(n2+1)%N][n3];
+                        w = arg(exp(complex<float>(0., p2 - p1))) + arg(exp(complex<float>(0., p3 - p2)))
+                          + arg(exp(complex<float>(0., p4 - p3))) + arg(exp(complex<float>(0., p1 - p4)));
+                        if (w > 0) { v1 += w; } else { v2 += w; }
+                    }
+                }
+            }
+            
+            return vector<double>{v1/(2*PI*N*N*L), v2/(2*PI*N*N*L)};
         }
 
         const float onsite_energy(int n1, int n2, int n3, int s) {
